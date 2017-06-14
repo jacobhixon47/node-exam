befa21a8b21f655ecee468f5aa3498257a2905df
Jacob Hixon <jacobhixon47@gmail.com>
2017-06-14 05:00:43 -0700

    properly counts tags, does not return object properly to front.

--- find-and-count.js
+++ find-and-count.js
@@ -2,29 +2,55 @@ const fs = require('fs');
 const path = require('path');
 
 var dir = __dirname + "/data";
+var tagCounts = {};
 
-module.exports = (userTags) => {
-  // console.log("TAGS: " + userTags);
+var checkForTags = (object, key) => {
+  // if json object has property key "tags"
+  if (object[key]) {
+    console.log("//// TAGS FOUND ////\n-------> LOADING TAGS....");
+    object[key].forEach( (tag) => {
+      console.log("TAG: " + tag);
+      console.log("PART OF SEARCH? " + Object.keys(tagCounts).includes(tag));
+      if (Object.keys(tagCounts).includes(tag)) {
+        tagCounts[tag] += 1;
+        for (var tg in tagCounts) {
+          console.log(tg + ": " + tagCounts[tg]);
+        }
+      }
+    });
+  }
+  for (var prop in object) {
+    if (prop !== key && typeof object[prop] === 'object') {
+      console.log("----------> CHECKING " + prop + " FOR TAGS...")
+      checkForTags(object[prop], key);
+    }
+  }
+};
 
+module.exports = (userTags, callback) => {
+  userTags.forEach( (tag) => {
+    tagCounts[tag] = 0;
+  });
   fs.readdir(dir, (err, fileList) => {
-    let count = 0;
     if (err) return console.error(err);
     let list = fileList.filter( (file) => {
       return path.extname(file) === ".json";
     });
     list.forEach( (file) => {
       fs.readFile(__dirname + '/data/' + file, (err, data) => {
-        if (err) return console.error(err);
-        // parse json and catch errors if invalid
-        try {
-          var myjson = JSON.parse(data);
-        } catch (error) {
-          return console.error(error);
+        if (err) {
+          return console.error(err);
+        } else {
+          // parse json and catch errors if invalid
+          try {
+            var myjson = JSON.parse(data);
+            checkForTags(myjson, "tags");
+          } catch (error) {
+            return console.error(error);
+          }
         }
-        console.log(myjson);
       });
-      // count++;
-      // console.log('+1');
     });
+    callback(null, tagCounts);
   });
 }
--- program.js
+++ program.js
@@ -1,4 +1,7 @@
 const findAndCount = require('./find-and-count');
 
 let tags = process.argv[2].split(',');
-findAndCount(tags);
+
+findAndCount(tags, (err, count) => {
+  console.log("COUNT: " + count);
+});
