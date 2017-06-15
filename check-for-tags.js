26ea265654b4aae8657041b8b86d101824db36a1
Jacob Hixon <jacobhixon47@gmail.com>
2017-06-14 17:11:48 -0700

    safe commit before cache work --- add lots of notes, dry up some, convert finalCount from hash to array (and adjust processes accordingly)

---
+++ check-for-tags.js
@@ -0,0 +1,26 @@
+module.exports = (object, counts, callback) => {
+  // set local instance of the tag counts
+  var tagCounts = counts;
+  // if tags property exists in current scope
+  if (object["tags"]) {
+    // sort through each tag in current tags object
+    object["tags"].forEach( (tag) => {
+      // look through tagCounts tags, see if any match this tag
+      for (var i = 0; i < tagCounts.length; i++) {
+        // if this tag matches a tag in the count, add 1 to the count of that tag
+        if (tagCounts[i][0] === tag) {
+          tagCounts[i][1] += 1;
+        }
+      }
+    });
+  }
+  for (var prop in object) {
+    // check if each property is an object
+    if (prop !== "tags" && typeof object[prop] === 'object') {
+      // if property is an object, run this same checker method on it (recursive)
+      module.exports(object[prop], tagCounts, () => {});
+    }
+  }
+  // run callback to push count back up to main program
+  callback(tagCounts);
+};
--- program.js
+++ program.js
@@ -1,7 +1,78 @@
-const findAndCount = require('./find-and-count');
+const fs = require('fs');
+const path = require('path');
+const checkForTags = require('./check-for-tags');
 
-let tags = process.argv[2].split(',');
+let dir = __dirname + '/data/';
+let finalCounts = [];
+let tags = [];
 
-findAndCount(tags, (err, count) => {
-  console.log("COUNT: " + count);
+// -------- PREP --------
+
+// create method so it can be called in either conditional below (DRY)
+var createTagCounts = (list) => {
+  list.forEach( (tag) => {
+    if (tag.length >= 1) {
+      finalCounts.push([tag, 0]);
+    }
+  });
+}
+// check if CLI argument was supplied
+if (process.argv[2]) {
+  tags = process.argv[2].split(',');
+  createTagCounts(tags);
+} else {
+  // if no CLI args, use tags from sample tags.txt file
+  fs.readFile(__dirname + '/tags.txt', 'utf8', (err, data) => {
+    tags = data.split('\n');
+    createTagCounts(tags);
+  });
+}
+
+// ---- MAIN FUNCTIONALITY ------
+
+// read /data/ directory and list files
+fs.readdir(dir, (err, fileList) => {
+  var fileCount = 0;
+  if (err) return console.error(err);
+  // filter fileList for only files with .json extension
+  let list = fileList.filter( (file) => {
+    return path.extname(file) === ".json";
+  });
+  // loop through each .json file
+  list.forEach( (file) => {
+    // read the file (async)
+    fs.readFile(__dirname + '/data/' + file, (err, data) => {
+      // after file is read, evaluate data
+      console.log('-------> CHECKING /data/' + file);
+      if (err) {
+        return console.error(err);
+      } else {
+        // parse json and catch errors if invalid
+        try {
+          var myjson = JSON.parse(data);
+          // check object for tags
+          checkForTags(myjson, finalCounts, (returnedCounts) => {
+            finalCounts = returnedCounts;
+          });
+        } catch (error) {
+          // catch invalid json
+          return console.error("\n" + error + "\n\n----------> Skipping file...");
+        } finally {
+          fileCount++;
+          // if all relevant files have been checked
+          if (fileCount === list.length) {
+            console.log("\n-------> DONE CHECKING FILES!\n");
+            // sort tags by count (highest to lowest)
+            finalCounts.sort( (a, b) => {
+              return b[1] - a[1];
+            });
+            // print each tag and its count to the console
+            finalCounts.forEach( (count) => {
+              console.log(count[0] + ": " + count[1]);
+            });
+          }
+        }
+      }
+    });
+  });
 });
